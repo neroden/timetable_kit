61eb623d83cc8f6c3fdeb675d783cf5eb8d2a848
Nathanael Nerode <ncn_mail10@fastmail.fm>
2022-03-03 13:22:55 -0500

    Fix docstring style (feed_enhanced.py)

--- feed_enhanced.py
+++ feed_enhanced.py
@@ -5,6 +5,7 @@
 
 """
 This module monkey-patches the Feed class from gtfs_kit to add methods.
+
 The additions are primarily filter methods: designed to take a feed and filter
 a bunch of data out to make a *smaller* feed which is quicker to do future processing on.
 There is also an extraction method to pull out a single trip record from a reduced feed,
@@ -17,15 +18,16 @@ from timetable_errors import NoTripError
 from timetable_errors import TwoTripsError
 
 def filter_by_dates(self, first_date, last_date):
-    '''
+    """
     Filter the entire feed to remove anything before first_date or after last_date
+
     Returns a new filtered feed (original feed unchanged)
     Must be in GTFS date format (string of YYYYMMDD)
     - Filters calendar (direct)
     - Filters trips (by service_ids in calendar)
     - FIXME: no other second-layer filtering
     Used for nearly all functions to get timetables effective for a single date
-    '''
+    """
     # N.B. Python's default string lexical compare is fine for GTFS date strings
     new_feed = self.copy()
     # Calendar must stop on or after the first date of the period...
@@ -40,8 +42,9 @@ def filter_by_dates(self, first_date, last_date):
     return new_feed
 
 def filter_by_route_ids(self, route_ids):
-    '''
+    """
     Filter the entire feed to include only the specified route_ids (a list)
+
     Returns a new filtered feed (original feed unchanged)
     - filters routes and trips (direct)
     - filters calendar (by service_ids in trips)
@@ -49,7 +52,7 @@ def filter_by_route_ids(self, route_ids):
     - filtered trips is used for timetable generation
     - filtered routes is not currently used
     - filtered calendar and trips are used in compare_similar_services
-    '''
+    """
     new_feed = self.copy()
     filtered_trips = self.trips[self.trips.route_id.isin(route_ids)]
     new_feed.trips = filtered_trips
@@ -61,14 +64,15 @@ def filter_by_route_ids(self, route_ids):
     return new_feed
 
 def filter_by_service_ids(self, service_ids):
-    '''
+    """
     Filter the entire feed to include only the specified service_ids (a list)
+
     Returns a new filtered feed (original feed unchanged)
     - filters calendar and trips (direct)
     - FIXME: no second-layer filtering
     - filtered calendar is used to extract the days of week for a service in the single trip printer
     - filtered trips is not currently used
-    '''
+    """
     new_feed = self.copy()
     # First the calendar
     new_feed.calendar = self.calendar[self.calendar.service_id.isin(service_ids)]
@@ -79,13 +83,14 @@ def filter_by_service_ids(self, service_ids):
     return new_feed
 
 def filter_bad_service_ids(self, bad_service_ids):
-    '''
+    """
     Filter the entire feed to remove specified bad service_ids (a list)
+
     Returns a new filtered feed (original feed unchanged)
     - filters calendar and trips (direct)
     - filtered trips is used to avoid known-bad data
     - filtered calender is not currently used
-    '''
+    """
     new_feed = self.copy()
     # First filter the trips.
     filtered_trips = self.trips[self.trips.service_id.isin(bad_service_ids).apply(not_)]
@@ -96,8 +101,9 @@ def filter_bad_service_ids(self, bad_service_ids):
     return new_feed
 
 def filter_by_trip_short_names(self, trip_short_names):
-    '''
+    """
     Filter the entire feed to include only services with the specified trip_short_names (a list)
+
     Amtrak "trip short name" is in fact an Amtrak train or bus number
     Returns a new filtered feed (original feed unchanged)
     - filters trips (direct)
@@ -106,7 +112,7 @@ def filter_by_trip_short_names(self, trip_short_names):
     Usually used with one-element list to get a single trip
     - filtered trips is used for retrieving a single trip_id, or a trip record
     - filtered stop_times is used for getting single trip stop times, but requires unique trip_id
-    '''
+    """
     new_feed = self.copy()
     # First filter the trips
     filtered_trips = self.trips[self.trips.trip_short_name.isin(trip_short_names)]
@@ -117,13 +123,14 @@ def filter_by_trip_short_names(self, trip_short_names):
     return new_feed
 
 def filter_by_trip_ids(self, trip_ids):
-    '''
+    """
     Filter the entire feed to include only services with the specified trip_ids (a list)
+
     Returns a new filtered feed (original feed unchanged)
     - filters trips and stop_times (direct)
     - FIXME: no second-layer filtering
     Usually used with one-element list to get a single trip
-    '''
+    """
     new_feed = self.copy()
     # First filter the trips
     filtered_trips = self.trips[self.trips.trip_id.isin(trip_ids)]
@@ -134,12 +141,13 @@ def filter_by_trip_ids(self, trip_ids):
     return new_feed
 
 def get_single_trip(self):
-    '''
-    With no arguments:
+    """
+    If this feed contains only one trip, return the trip.
+
     -- If there's only one trip, return it.
     -- Otherwise throw a suitable error.
     -- Used for checking that we've reduced the trips table enough but not too much.
-    '''
+    """
     num_rows = self.trips.shape[0]
     if (num_rows == 0):
         raise NoTripError("Expected single trip: no trips in filtered trips table", self.trips)
@@ -149,10 +157,11 @@ def get_single_trip(self):
     return this_trip_today
 
 def get_single_trip_stop_times(self, trip_id):
-    '''
-    Get stop times for a single trip sorted into the correct order
+    """
+    Get stop times for a single trip -- sorted into the correct order
+
     May need improved interface... not clear
-    '''
+    """
     filtered_feed = self.filter_by_trip_ids([trip_id])
     stop_times_1 = filtered_feed.stop_times
     stop_times_2 = stop_times_1.set_index("stop_sequence")
@@ -160,12 +169,14 @@ def get_single_trip_stop_times(self, trip_id):
     return stop_times_3
 
 def get_trip_short_name(self, trip_id):
-    '''
+    """
     Given a trip_id, recover the trip_short_name
+
     Since trip_ids are supposed to be unique, this should be an easy process.
+
     Very useful for debugging since trip_short_name is human-meaningful,
     and trip_id isn't.
-    '''
+    """
     my_trips = self.trips[self.trips["trip_id"] == trip_id]
     if (my_trips.shape[0] == 0):
         raise NoTripError("No trip with trip_id ", trip_id)
