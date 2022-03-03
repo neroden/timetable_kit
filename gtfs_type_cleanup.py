a0606ab33f38172ffdfe5cd338f663799ee14575
Nathanael Nerode <ncn_mail10@fastmail.fm>
2022-03-03 13:26:11 -0500

    Docstring style fixes (gtfs_type_cleanup.py)

--- gtfs_type_cleanup.py
+++ gtfs_type_cleanup.py
@@ -3,24 +3,27 @@
 # Part of timetable_kit
 # Copyright 2021, 2022 Nathanael Nerode.  Licensed under GNU Affero GPL v.3 or later.
 
-'''
+"""
 Gtfs_kit is an excellent tool, but it puts practically everything into string types;
-this is not useful for later work.  These subroutines convert certain columns to integers.
-'''
+this is not always useful for later work.  These subroutines convert certain columns to integers.
+"""
 
 # Other people's packages
 import pandas as pd
 import gtfs_kit as gk
 
 def type_corrected_agency(agency):
-    "Return copy of agency DataFrame with integer types in appropriate columns.  Sort by agency_id."
-    "Take raw agency DataFrame, type-correct the agency ID, sort by it.  Can be repeated."
+    """
+    Return copy of agency DataFrame with integer types in appropriate columns.  Sort by agency_id.
+
+    Take raw agency DataFrame, type-correct the agency ID, sort by it.  Can be repeated.
+    """
     new_agency = agency.astype({'agency_id': 'int32'})
     new_agency_2 = new_agency.sort_values(by=['agency_id'])
     return new_agency_2
 
 def type_corrected_calendar(calendar):
-    "Return copy of calendar DataFrame with integer types in appropriate columns.  Sort by service_id."
+    """Return copy of calendar DataFrame with integer types in appropriate columns.  Sort by service_id."""
     new_calendar = calendar.astype({'service_id': 'int32',
                                     'monday': 'bool',
                                     'tuesday': 'bool',
@@ -36,7 +39,7 @@ def type_corrected_calendar(calendar):
     return new_calendar_2
 
 def type_corrected_routes(routes):
-    "Return copy of routes DataFrame with integer types in appropriate columns.  Sort by route_id."
+    """Return copy of routes DataFrame with integer types in appropriate columns.  Sort by route_id."""
     new_routes = routes.astype({'route_id': 'int32',
                                 'agency_id': 'int32',
                                 'route_type': 'int32' # 2 for trains, 3 for buses
@@ -45,7 +48,7 @@ def type_corrected_routes(routes):
     return new_routes_2
 
 def type_corrected_stop_times(stop_times):
-    "Return copy of stop_times with integer types in appropriate columns.  Sort by trip_id."
+    """Return copy of stop_times with integer types in appropriate columns.  Sort by trip_id."""
     new_stop_times = stop_times.astype({'trip_id': 'int64',
                                         'stop_sequence': 'int32',
                                         'pickup_type': 'int32', # 1 = dropoff only, 2/3 = flag stop
@@ -55,8 +58,11 @@ def type_corrected_stop_times(stop_times):
     return new_stop_times_2
 
 def type_corrected_trips(trips):
-    "Return copy of trips with integer types in appropriate columns.  Do not sort."
-    "Note that trip_id must be int64, as it tends to be enormous."
+    """
+    Return copy of trips with integer types in appropriate columns.  Do not sort.
+
+    Note that trip_id must be int64, as it tends to be enormous.
+    """
     new_trips = trips.astype({'route_id': 'int32',
                               'service_id':'int32',
                               'trip_id': 'int64',
@@ -67,7 +73,7 @@ def type_corrected_trips(trips):
 
 # Fix types on tables: integers where appropriate
 def fix(feed):
-    "Fixes types on all tables in the feed, in place."
+    """Fixes types on all tables in the feed, in place."""
     new_agency = type_corrected_agency(feed.agency)
     feed.agency = new_agency
     new_calendar = type_corrected_calendar(feed.calendar)
