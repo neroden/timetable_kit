b28775d0a14120c0af3dc7414e92b9ff7110109b
Nathanael Nerode <ncn_mail10@fastmail.fm>
2022-03-11 21:30:54 -0500

    amtrak/get_gtfs.py: auto-extract (for the benefit of human eyes)

--- timetable_kit/amtrak/get_gtfs.py
+++ timetable_kit/amtrak/get_gtfs.py
@@ -13,6 +13,8 @@ I need to handle the local file munging better, but this is a start.
 from pathlib import Path
 import requests
 
+from zipfile import ZipFile
+
 # These are mine
 # from timetable_kit import amtrak
 
@@ -21,6 +23,7 @@ canonical_gtfs_url="https://content.amtrak.com/content/gtfs/GTFS.zip"
 
 module_location = Path(__file__).parent
 gtfs_zip_local_path = module_location / "GTFS.zip"
+gtfs_unzipped_local_path = module_location / "gtfs"
 
 # This cannot possibly be the right way to do this.
 # wget is probably cleanest, but adds yet another dependency
@@ -44,7 +47,22 @@ def save_gtfs(gtfs_zip):
     with open(gtfs_zip_local_path, 'wb') as binary_file:
         binary_file.write( gtfs_zip )
 
+def unzip_gtfs():
+    """
+    Extract Amtrak's GTFS file from a canonical local location to a canonical local location.
+
+    This isn't used directly by the program; this is just for human inspection.
+    """
+    with ZipFile(gtfs_zip_local_path, 'r') as my_zip:
+        if not ( gtfs_unzipped_local_path.exists() ):
+            gtfs_unzipped_local_path.mkdir(parents=True)
+        my_zip.extractall(path=gtfs_unzipped_local_path)
+        print ( "Extracted to " + str(gtfs_unzipped_local_path) )
+    return
+
+# MAIN PROGRAM
 if __name__ == "__main__":
     save_gtfs( download_gtfs() )
     print ("Amtrak GTFS saved at " + str(gtfs_zip_local_path) )
+    unzip_gtfs()
     quit()
