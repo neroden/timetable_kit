0f6000783621ca08b2a2b65eda97d987d06a594c
Nathanael Nerode <ncn_mail10@fastmail.fm>
2022-03-25 10:23:32 -0400

    Add homebrewed test for trains with checked baggage to amtrak/special_data.py

--- timetable_kit/amtrak/special_data.py
+++ timetable_kit/amtrak/special_data.py
@@ -7,7 +7,7 @@
 This module includes Amtrak *data* which isn't provided by Amtrak.
 
 This includes the list of which stations are major,
-which trains carry checked baggage (not implemented FIXME),
+which trains carry checked baggage,
 known problems with Amtrak's GTFS data, and similar.
 
 (Functions for extracting data from Amtrak's JSON stations database are elsewhere.)
@@ -16,8 +16,88 @@ known problems with Amtrak's GTFS data, and similar.
 # GLOBAL VARIABLES
 #
 # Known problems in Amtrak data
-global_bad_service_ids = [2819372, # Cardinal one-day service when it doesn't run on that day
-                         ]
+global_bad_service_ids = [2819372, ] # Cardinal one-day service when it doesn't run on that day
+
+
+def is_bus(train_number):
+    """Is this a bus?"""
+    if int(train_number) >= 3000:
+        return True
+    return False
+
+# Parentheses makes a TUPLE.
+sleeper_trains = set ( (   "1", "2", # SL
+    "21", "22", "421", "422", # TE -- also 321/322?
+    "3", "4", # SWC
+    "5", "6", # CZ
+    "7", "8", "27", "28", # EB
+    "11", "14", # CS
+    "19", "20", # Crescent
+    "29", "30", # CL
+    "48", "49", "448", "449", # LSL
+    "50", "51", # Cardinal
+    "52","53", # Auto Train
+    "58", "59", # CONO
+    "65", "66", "67", # Night Owl
+    "91", "92", # Silver Star
+    "97", "98", # Silver Meteor
+    ) )
+
+def is_sleeper_train(train_number):
+    """Does this train have sleeper cars?"""
+    return train_number in sleeper_trains
+
+# Trains with checked baggage cars.
+# This one really does need to be right; I can't get it from Amtrak data.
+
+# Oookay.  Pacific Surfliners change their numbers fairly frequently.
+# There are two ranges:
+# 561 through 595 (NB) or 562 through 596 (SB) for "short" trains south of LA
+# 761 through 795 (NB) or 762 through 796 (SB) for "long" trains north of LA
+# We do 560-599 and 760-799 for completeness.  Note that range excludes the last item.
+pacific_surfliner_trains = [ str(item) for item in [*range(560,600), *range(760,800)] ]
+
+# Similar issue with San Joaquins.
+# Lowest numbered is #701.  Highest numbered is #719.
+# There is no #700, and #699 is a Downeaster.
+# While #720 is a Capitol Corridor to Sacramento.
+san_joaquins_trains = [ str(item) for item in range(700,720) ]
+
+# Similar issue with Cascades.
+# Lowest numbered Cascades is #500.  Highest is #519.
+# And #520 is a Sacramento Capitol Corridor train.
+# And #499 is a Springfield - New Haven train.
+cascades_trains = [ str(item) for item in range(500,520) ]
+
+# Similar issue with Hiawatha.
+# Lowest numbered Hiawatha is 329; highest numbered is 343.
+# However, #311-316 are Kansas City to St Louis.
+# However, #350 is a Wolverine to Pontiac.
+# We do 320-349.
+hiawatha_trains = [ str(item) for item in range(320,350) ]
+
+other_checked_baggage_day_trains = (    "42", "43", # Pennsylvanian
+    "79","80", # Carolinian
+    "73","74","75","76", # Piedmont
+    "89","90", # Palmetto
+    )
+
+# Assemble these trains as a set.  Ugly!
+checked_baggage_trains = set( [ *sleeper_trains,
+    *pacific_surfliner_trains,
+    *san_joaquins_trains,
+    *cascades_trains,
+    *hiawatha_trains,
+    *other_checked_baggage_day_trains,
+    ] )
+
+def train_has_checked_baggage(trip_short_name: str) -> bool:
+    """
+    Given a trip_short_name (train number), return "True" if it has checked baggage and "False" if not.
+
+    This is based on crowdsourced data since Amtrak doesn't have a machine-readable way to get it.
+    """
+    return (trip_short_name in checked_baggage_trains)
 
 # "Major stations".  This is for timetable styling: making them bigger and bolder.
 # This should really be per-timetable but this is a start
@@ -83,29 +163,9 @@ def is_standard_major_station(station_code):
     """Is a station on the list of standard 'major stations' for Amtrak?"""
     return (station_code in major_stations_list)
 
-def is_bus(train_number):
-    """Is this a bus?"""
-    if int(train_number) >= 3000:
-        return True
-    return False
+# TESTING
+if __name__ == "__main__":
+    print (pacific_surfliner_trains)
+    print (train_has_checked_baggage("1"))
+    print (train_has_checked_baggage("5"))
 
-sleeper_trains = ( "52","53", # Auto Train
-    "5", "6", # CZ
-    "29", "30", # CL
-    "50", "51", # Cardinal
-    "58", "59", # CONO
-    "11", "14", # CS
-    "19", "20", # Crescent
-    "7", "8", "27", "28", # EB
-    "48", "49", "448", "449", # LSL
-    "65", "66", "67", # Night Owl
-    "97", "98", # Silver Meteor
-    "91", "92", # Silver Star
-    "3", "4", # SWC
-    "1", "2", # SL
-    "21", "22", "421", "422", # TE -- also 321/322?
-    )
-
-def is_sleeper_train(train_number):
-    """Does this train have sleeper cars?"""
-    return train_number in sleeper_trains
