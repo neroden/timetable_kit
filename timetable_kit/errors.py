44809b44e4acfb9cb8d54725559d70dcdf881516
Nathanael Nerode <ncn_mail10@fastmail.fm>
2022-04-10 11:45:11 -0400

    Run errors.py through black

--- timetable_kit/errors.py
+++ timetable_kit/errors.py
@@ -18,20 +18,26 @@ FutureCodeError
 class GTFSError(ValueError):
     """Exception for unexpected data in the GTFS feed."""
 
+
 class NoStopError(GTFSError):
     """Exception when a train doesn't stop at a station.  Frequently caught."""
+
 
 class TwoStopsError(GTFSError):
     """Exception when a train stops at a station twice.  Frequently caught."""
 
+
 class NoTripError(GTFSError):
     """Exception when no trips where one is expected."""
+
 
 class TwoTripsError(GTFSError):
     """Exception when several trips where one is expected."""
 
+
 class InputError(ValueError):
     """Exception for unexpected data passed in by the user."""
+
 
 class FutureCodeError(RuntimeError):
     """Exception for calling functions which are unimplemented stubs."""
