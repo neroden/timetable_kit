affdaf3b1d379bf4195a0d19520b940edb8d4864
Nathanael Nerode <ncn_mail10@fastmail.fm>
2022-06-16 10:42:20 -0400

    Working version of merge_gtfs.py (without validation or filtering)

--- timetable_kit/merge_gtfs.py
+++ timetable_kit/merge_gtfs.py
@@ -5,28 +5,33 @@
 
 """
 Merge two GTFS feeds.
+
+Currently does no validation.
 """
 
-import sys # for argv
+import sys  # for argv
 from pathlib import Path
 
 import pandas as pd
-import gtfs_kit as gk
+import gtfs_kit
 
-def index_by_ids(old_feed: gk.Feed, / ) -> gk.Feed:
+
+def index_by_ids(old_feed: gtfs_kit.Feed, /) -> gtfs_kit.Feed:
     """
     Return a copy of the feed, with all the tables which have unique IDs indexed by them.
+
+    This will be a slightly broken Feed, as gtfs_kit.Feed does not expect the indexing.
     """
     feed = old_feed.copy()
     # Required files.
     # Note that the agency_id field is optional.  FIXME.
-    feed.agency.set_index("agency_id", inplace = True)
-    feed.stops.set_index("stop_id", inplace = True)
-    feed.routes.set_index("route_id", inplace = True)
-    feed.trips.set_index("trip_id", inplace = True)
-    feed.stop_times.set_index("trip_id", inplace = True)
+    feed.agency.set_index("agency_id", inplace=True)
+    feed.stops.set_index("stop_id", inplace=True)
+    feed.routes.set_index("route_id", inplace=True)
+    feed.trips.set_index("trip_id", inplace=True)
+    feed.stop_times.set_index("trip_id", inplace=True)
     if feed.calendar is not None:
-        feed.calendar.set_index("service_id", inplace = True)
+        feed.calendar.set_index("service_id", inplace=True)
     if feed.calendar_dates is not None:
         feed.calendar_dates.set_index("service_id", inplace=True)
     if feed.fare_attributes is not None:
@@ -49,43 +54,43 @@ def index_by_ids(old_feed: gk.Feed, / ) -> gk.Feed:
         feed.attributions.set_index("attribution_id", inplace=True)
     return feed
 
-def merge_feed(feed_a, feed_b) -> gk.Feed:
+
+# This is the set of all table names in the feed
+table_names = set(gtfs_kit.constants.FEED_ATTRS) - set(["dist_units"])
+
+
+def merge_feed(feed_a, feed_b) -> gtfs_kit.Feed:
     """
     Merge two feeds.  Return the merged feed.
 
-    All the _id fields must be unique.  Filter your feed first if you have duplicates.
+    Does not do ANY checking for uniqueness or duplicates.
+    To get a valid feed as a result, all the _id fields must be unique.
+    Filter your feeds first if you have duplicates.
     """
 
+    # gtfs_kit.constants.FEED_ATTRS_1
     # First, copy the incoming feeds for index manipulation.
     # They are now prepared to be concatenated.
     a = index_by_ids(feed_a)
     b = index_by_ids(feed_b)
-    feed_out = gk.Feed(dist_units=a.dist_units)
+    feed_out = gtfs_kit.Feed(dist_units=a.dist_units)
     # FIXME: This uses the dist_units from A with no conversion attempt for B
 
-    # FIXME: This is not working.  Try using database join instead :-(
-
-    # Note that we have to hack into _trips, _calendar, and _calendar_dates
-    # Because the decorated versions can't handle concat
-    feed_out.agency = pd.concat([a.agency, b.agency], verify_integrity=True)
-    feed_out.stops = pd.concat([a.stops, b.stops], verify_integrity=False) # for testing
-    feed_out.routes = pd.concat([a.routes, b.routes], verify_integrity=True)
-    feed_out.trips = pd.concat([a._trips, b._trips], verify_integrity=True)
-    feed_out.stop_times = pd.concat([a.stop_times, b.stop_times], verify_integrity=True)
-    feed_out.calendar = pd.concat([a._calendar, b._calendar], verify_integrity=True)
-    feed_out.calendar_dates = pd.concat([a._calendar_dates], b._calendar_dates, verify_integrity=True)
-    feed_out.fare_attributes = pd.concat([a.fare_attributes], b.fare_attributes, verify_integrity=True)
-    feed_out.fare_rules = pd.concat([a.fare_rules, b.fare_rules], verify_integrity=True)
-    feed_out.shapes = pd.concat([a.shapes, b.shapes], verify_integrity=True)
-    feed_out.frequencies = pd.concat([a.frequencies, b.frequencies], verify_integrity=True)
-    feed_out.transfers = pd.concat([a.transfers, b.transfers], verify_integrity=True)
-    feed_out.feed_info = pd.concat([a.feed_info, b.feed_info], verify_integrity=True)
-    feed_out.attributions = pd.concat([a.attributions, b.attributions], verify_integrity=True)
-    # FIXME: this does not handle the tables not handled by gtfs_kit
-
-    # NOTE: this leaves the pandas tables in feed_out indexed, which may be wrong.  FIXME
+    for table_name in table_names:
+        source_tables = [getattr(a, table_name), getattr(b, table_name)]
+        match source_tables:
+            case [None, None]:
+                table_value = None
+            case [value, None]:
+                table_value = value
+            case [None, value]:
+                table_value = value
+            case _:
+                table_value = pd.concat(source_tables).reset_index()
+        setattr(feed_out, table_name, table_value)
 
     return feed_out
+
 
 # MAIN PROGRAM
 if __name__ == "__main__":
@@ -97,8 +102,8 @@ if __name__ == "__main__":
     feed_b_filename = sys.argv[2]
     filename_out = sys.argv[3]
 
-    feed_a = gk.read_feed(Path(feed_a_filename), dist_units="mi")
-    feed_b = gk.read_feed(Path(feed_b_filename), dist_units="mi")
+    feed_a = gtfs_kit.read_feed(Path(feed_a_filename), dist_units="mi")
+    feed_b = gtfs_kit.read_feed(Path(feed_b_filename), dist_units="mi")
 
     feed_out = merge_feed(feed_a, feed_b)
     feed_out.write(Path(filename_out))
