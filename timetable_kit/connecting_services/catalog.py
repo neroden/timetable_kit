34c432a165fe6ca6d6f180d25a308a2d84742e26
Nathanael Nerode <ncn_mail10@fastmail.fm>
2022-12-28 20:17:13 -0500

    Connecting services: minor work-in-progress

--- timetable_kit/connecting_services/catalog.py
+++ timetable_kit/connecting_services/catalog.py
@@ -8,7 +8,45 @@ This module contains the list of known connecting services, with
 their icon files, CSS files, CSS class names, etc.
 
 The exported data is connecting_services_data.
+
+To do: consider converting this to a .csv file and reading it dynamically.
 """
+
+# This isn't real, it's a template for making new ones
+# Note that "title" is the mouseover
+# "alt" is for when we can't display images at all
+# "full_name" is for use in the key at the bottom
+_generic_dict = {
+    "src": "generic.svg",
+    "css_file": "generic.css",
+    "class": "generic-img",
+    "alt": "Generic",
+    "title": "Generic",
+    "url": "https://www.generic.org/",
+    "full_name": "Generic Agency",
+}
+
+# NEC connecting agencies: North to south.
+
+_njt_dict = {
+    "src": "NJT_logo.svg",
+    "css_file": "NJT_logo.css",
+    "class": "njt-logo-img",
+    "alt": "NJT",
+    "title": "NJ Transit",
+    "url": "https://www.njtransit.com/",
+    "full_name": "NJ Transit",
+}
+
+_septa_dict =
+    "src": "SEPTA.svg",
+    "css_file": "SEPTA.css",
+    "class": "septa-img",
+    "alt": "SEPTA",
+    "title": "SEPTA",
+    "url": "https://septa.org/",
+    "full_name": "SEPTA",
+}
 
 _marc_dict = {
     "src": "MARC_train.svg",
@@ -30,8 +68,19 @@ _baltimore_lrt_dict = {
     "full_name": "Baltimore Light Rail Link",
 }
 
+_wmata_dict = {
+    "src": "WMATA_Metro_Logo.svg",
+    "css_file": "WMATA_Metro_Logo.css",
+    "class": "wmata-metro-img",
+    "alt": "WMATA",
+    "title": "WMATA",
+    "url": "https://www.wmata.com/",
+    "full_name": "WMATA",
+}
+
 # And now the "dictionary of dictionaries":
 connecting_services_data = {
     "marc": _marc_dict,
     "baltimore_lrt": _baltimore_lrt_dict,
+    
 }
