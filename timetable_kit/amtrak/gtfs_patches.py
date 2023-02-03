f7b7560c9fc12eae9d1fb655066e4a75e59654d0
Nathanael Nerode <ncn_mail10@fastmail.fm>
2023-02-03 11:50:26 -0500

    Revert "Remove Coast Starlight patch no longer needed with 3 Feb 2023 timetable"
    This reverts commit 32b071a24dd62a6b7f286dbbc5a74e17ba1ff058.
    
    The patch is still needed for timetables for services after March 18

--- timetable_kit/amtrak/gtfs_patches.py
+++ timetable_kit/amtrak/gtfs_patches.py
@@ -13,9 +13,6 @@ from timetable_kit import feed_enhanced
 from timetable_kit.debug import debug_print
 
 
-
-
-
 def patch_feed(feed):
     """
     Take an Amtrak feed and patch it for known errors.
@@ -26,46 +23,45 @@ def patch_feed(feed):
     new_feed = feed.copy()
     new_calendar = new_feed.calendar
 
-    # Coast Starlight FIXED 3 Feb 2023
     # Coast Starlight: two bogus errors!
     # 11 is missing Saturday and has two other identical calendars;
     # 14 has three identical calendars
 
     # First find the route id:
-    #for index in new_feed.routes.index:
-    #   if new_feed.routes.loc[index, "route_long_name"] == "Coast Starlight":
-    #       cs_route_id = new_feed.routes.loc[index, "route_id"]
-    #       break
-    #debug_print(1, "Coast Starlight route_id: ", cs_route_id)
+    for index in new_feed.routes.index:
+        if new_feed.routes.loc[index, "route_long_name"] == "Coast Starlight":
+            cs_route_id = new_feed.routes.loc[index, "route_id"]
+            break
+    debug_print(1, "Coast Starlight route_id: ", cs_route_id)
     # Then find the service ids:
-    #service_ids = set()
-    #for index in new_feed.trips.index:
-    #   if new_feed.trips.loc[index, "route_id"] == cs_route_id:
-    #       service_ids.add(new_feed.trips.loc[index, "service_id"])
+    service_ids = set()
+    for index in new_feed.trips.index:
+        if new_feed.trips.loc[index, "route_id"] == cs_route_id:
+            service_ids.add(new_feed.trips.loc[index, "service_id"])
     # Now cycle through the calendars.
     # First find and drop the bogus calendars:
-    #for index in new_calendar.index:
-    #   if new_calendar.loc[index, "service_id"] in service_ids:  # Coast Starlight
-    #       if new_calendar.loc[index, "wednesday"] == 0:  # Not on Wednesday
-    #           new_calendar = new_calendar.drop(labels=[index], axis="index")
-    #           debug_print(
-    #               1, "Patched not-on-wednesday special out of Coast Starlight"
-    #           ),
-    #for index in new_calendar.index:
-    #    # Now patch the Wednesday calendars to be every-day-of-the-week:
-    #   # (This is the good calendar)
-    #   if new_calendar.loc[index, "service_id"] in service_ids:  # Coast Starlight
-    #       new_calendar.loc[index, "sunday"] = 1
-    #       new_calendar.loc[index, "monday"] = 1
-    #       new_calendar.loc[index, "tuesday"] = 1
-    #       new_calendar.loc[index, "wednesday"] = 1
-    #       new_calendar.loc[index, "thursday"] = 1
-    #       new_calendar.loc[index, "friday"] = 1
-    #       new_calendar.loc[index, "saturday"] = 1
-    #       debug_print(1, "Patched full week into Coast Starlight calendar")
+    for index in new_calendar.index:
+        if new_calendar.loc[index, "service_id"] in service_ids:  # Coast Starlight
+            if new_calendar.loc[index, "wednesday"] == 0:  # Not on Wednesday
+                new_calendar = new_calendar.drop(labels=[index], axis="index")
+                debug_print(
+                    1, "Patched not-on-wednesday special out of Coast Starlight"
+                ),
+    for index in new_calendar.index:
+        # Now patch the Wednesday calendars to be every-day-of-the-week:
+        # (This is the good calendar)
+        if new_calendar.loc[index, "service_id"] in service_ids:  # Coast Starlight
+            new_calendar.loc[index, "sunday"] = 1
+            new_calendar.loc[index, "monday"] = 1
+            new_calendar.loc[index, "tuesday"] = 1
+            new_calendar.loc[index, "wednesday"] = 1
+            new_calendar.loc[index, "thursday"] = 1
+            new_calendar.loc[index, "friday"] = 1
+            new_calendar.loc[index, "saturday"] = 1
+            debug_print(1, "Patched full week into Coast Starlight calendar")
 
     # And update with the new calendar, just in case it hadn't
-    #new_feed.calendar = new_calendar
+    new_feed.calendar = new_calendar
 
     # Toronto: incorrectly listed as "no pickups" in stop one.
     new_stop_times = new_feed.stop_times
