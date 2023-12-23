78d5fc685352aee3e2741320c77137f5bd0f4e7c
Nathanael Nerode <ncn_mail10@fastmail.fm>
2023-12-22 23:31:36 -0500

    Wrap the landscape/portrait rules in a @media print wrapper to squash complaints in web browsers.  Modify fast-test.sh.

--- timetable_kit/fast-test.sh
+++ timetable_kit/fast-test.sh
@@ -6,11 +6,17 @@
 
 # Test all four functional submodules
 
+# Test a very long table to make sure there's no overflow
 ./timetable.py --agency amtrak --spec empire-builder
+# Test noheader
 ./timetable.py --agency amtrak --spec vermont-to-upstate-ny
+# Test landscape, and coloring, and some other stuff
+./timetable.py --agency amtrak --spec grand-canyon
 
+# Test VIA
 ./timetable.py --agency via --spec ocean
 
+# Test Maple Leaf
 ./timetable.py --agency maple_leaf --spec maple-leaf
 
 # ./timetable.py --agency hartford --spec hartford-line-valley-flyer.list
