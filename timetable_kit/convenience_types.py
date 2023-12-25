c9622dc614c9b820cbafc339d152828dc631eee6
Nathanael Nerode <ncn_mail10@fastmail.fm>
2023-12-24 23:31:48 -0500

    Add convenience_types.py (unbreaks the program, now that feed_enhanced uses this)

---
+++ timetable_kit/convenience_types.py
@@ -0,0 +1,21 @@
+# convenience_types.py
+# Part of timetable_kit
+# Copyright 2022 Nathanael Nerode.  Licensed under GNU Affero GPL v.3 or later.
+
+"""
+Types used for extra type-checking
+"""
+
+# Do this after working our way through the codebase to fix all calls
+# from typing import NewType
+
+# Do this for now
+from typing import TypeAlias
+
+# This must be a date in YYYYMMDD format
+# type GTFSDate = NewType("GTFSDate", str)
+GTFSDate: TypeAlias = str
+
+# This must be one of the GTFS_DAYS listed in feed_enhanced.py
+# type GTFSDay = NewType("GTFSDay", str)
+GTFSDay: TypeAlias = str
