e5046a4018d79e0e50eae98e1a59b6964d3c3481
Nathanael Nerode <ncn_mail10@fastmail.fm>
2023-12-26 16:21:35 -0500

    Prep for transposed

--- timetable_kit/core.py
+++ timetable_kit/core.py
@@ -109,6 +109,8 @@ class TTSpec(object):
         This edits the dict in place.
         """
         self.aux.setdefault("for_rpa", False)
+        # Affects a lot of things.
+        self.aux.setdefault("transposed", False)
         # These are used when filling the timetable.
         self.aux.setdefault("train_numbers_side_by_side", False)
         self.aux.setdefault("dwell_secs_cutoff", 300)
@@ -793,6 +795,7 @@ def fill_tt_spec(
     train_numbers_side_by_side = bool(spec.aux["train_numbers_side_by_side"])
     use_bus_icon_in_cells = bool(spec.aux["use_bus_icon_in_cells"])
     box_time_characters = bool(spec.aux["box_time_characters"])
+    transposed = bool(spec.aux["transposed"])
 
     if "programmers_warning" in spec.aux:
         print("WARNING: ", spec.aux["programmers_warning"])
@@ -919,21 +922,25 @@ def fill_tt_spec(
         column_header_styling = ""  # default
         match column_key_str.lower():
             case "station" | "stations":
+                # in a span
                 column_header = text_presentation.get_station_column_header(
                     doing_html=doing_html
                 )
             case "services":
+                # in a span
                 column_header = text_presentation.get_services_column_header(
                     doing_html=doing_html
-                )  # in a span
+                )
             case "access":
+                # in a span
                 column_header = text_presentation.get_access_column_header(
                     doing_html=doing_html
-                )  # in a span
+                )
             case "timezone":
+                # in a span
                 column_header = text_presentation.get_timezone_column_header(
                     doing_html=doing_html
-                )  # in a span
+                )
             case _:
                 # it's actually a train, or several trains
                 # Check column options for reverse, days, ardp:
