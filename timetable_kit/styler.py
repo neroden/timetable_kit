908f2726141f259517fa96da7780658451f00725
Nathanael Nerode <ncn_mail10@fastmail.fm>
2023-12-28 17:24:34 -0500

    Work in progress

--- timetable_kit/styler.py
+++ timetable_kit/styler.py
@@ -6,20 +6,23 @@
 Created when PANDAS's Styler wasn't doing what I wanted.
 """
 
-import sys # for sys.exit
+import sys  # for sys.exit
 
 import pandas as pd
 from pandas import DataFrame
 
 # My packages
 from timetable_kit.debug import debug_print
+
 # Type to store laid-out timetable as a set of dataframes
 from timetable_kit.timetable_class import Timetable
+
 # For the Jinja templates
 from timetable_kit.load_resources import template_environment
 
 # Internal use: Jinja templates after loading
 _table_tpl: str | None = None
+
 
 def get_table_tpl():
     """Get the Jinja template for a whole table.
@@ -32,6 +35,7 @@ def get_table_tpl():
     assert _table_tpl is not None
     return _table_tpl
 
+
 def render(t: Timetable):
     """Render a timetable to HTML, using Jinja"""
     # Recall that Python dataframe data can be accessed as:
@@ -41,8 +45,9 @@ def render(t: Timetable):
     table_tpl = get_table_tpl()
     params = {
         "table_attributes": t.table_attributes,
-        "row_nums": t.row_nums,
-        "col_nums": t.col_nums,
+        "thead_row_nums": t.thead_row_nums,
+        "tbody_row_nums": t.tbody_row_nums,
+        "printable_col_nums": t.printable_col_nums,
         # These four are DataFrames
         "text": t.text,
         "classes": t.classes,
@@ -50,6 +55,4 @@ def render(t: Timetable):
         "attributes": t.attributes,
     }
     output = table_tpl.render(params)
-    print(output)
-    sys.exit(1)
-
+    return output
--- timetable_kit/timetable_class.py
+++ timetable_kit/timetable_class.py
@@ -11,8 +11,14 @@ from pandas import DataFrame
 
 from timetable_kit.debug import debug_print
 
+
 class Timetable:
-    """Represents a laid-out timetable with styling"""
+    """Represents a laid-out timetable with styling
+
+    This is used both before and after layout.
+    It is filled in by fill_tt_spec in an encapsulation-breaking way.
+    """
+
     def __init__(self, spec_df: pd.DataFrame) -> None:
         """Pass in the CSV part of a spec as a dataframe, which is used only for its shape"""
         (row_index, col_index) = spec_df.axes
@@ -32,12 +38,38 @@ class Timetable:
         self.attributes = pd.DataFrame(
             index=row_index.copy(deep=True), columns=col_index.copy(deep=True)
         )
-        # We will use the row_count and col_count later.
+        # These are used to tell Jinja when to stop looping.
+        # We will also use the row_count and col_count in the main loop to fill the timetable,
+        # but we currently pull those separately.  (FIXME)
         (self.row_count, self.col_count) = spec_df.shape
-        # These are needed by Jinja.
-        # Later, we'll split the rows into "head" and "body".  TODO
-        self.row_nums = range(0, self.row_count)
-        self.col_nums = range(0, self.col_count)
+
+        # These would be used naively by Jinja to loop, but we need something more complex...
+        # Instead, they are not currently used.
+        # self.row_nums = range(0, self.row_count)
+        # self.col_nums = range(0, self.col_count)
+
+        # These are used by Jinja to loop separately on <thead> and <tbody>.
+        # For now, we do it the simple way, though we could be more complex.
+        # Will require revision when transposed timetables are implemented (FIXME)
+        assert self.row_count >= 1
+        self.thead_row_nums = range(0, 1)
+        self.tbody_row_nums = range(1, self.row_count)
+        # This is used by Jinja to loop over columns
+        # while skipping the do-not-print column number 0.
+        # Will require revision when transposed timetables are implemented (FIXME)
+        assert self.col_count >= 1
+        self.printable_col_nums = range(1, self.col_count)
+
         debug_print(1, "Copied shape of spec.")
         # Things which go on the <table> tag
+        # We can get these from the TTSpec.  Fix after rearranging modules. FIXME
         self.table_attributes = ""
+
+    def write_csv_file(self, file: PathLike | str) -> None:
+        """Write this out as a CSV file at the given path.
+
+        Should run after fill_tt_spec.
+        """
+        # NOTE, need to include the header
+        selftext.to_csv(file, index=False, header=True)
+        debug_print(1, "CSV written to", file)
